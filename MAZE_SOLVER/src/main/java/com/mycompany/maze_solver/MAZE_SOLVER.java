5a0dd60b6607cf01b5f3ceec046a273e05bacb45
Ishwariya S <109616709+Ishwariya04@users.noreply.github.com>
2023-04-19 21:56:06 +0530

    Add files via upload

---
+++ MAZE_SOLVER/src/main/java/com/mycompany/maze_solver/MAZE_SOLVER.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.maze_solver;
+
+/**
+ *
+ * @author aishwarya
+ */
+public class MAZE_SOLVER {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
